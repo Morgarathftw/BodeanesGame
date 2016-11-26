7a4b2b1e4cc3c4edd5bbce3b74c87d0b7ff82a61
Morgarathftw <morgarathftw@gmail.com>
2016-11-27 00:33:21 +1300

    Added first mouse script

---
+++ BodeanesGame/Assets/Scripts/MouseBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseBehaviour : MonoBehaviour {
+
+    private Vector3 target;
+
+    // Use this for initialization
+    void Start () {
+        target = transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        float _fx = Mathf.Floor(target.x + 0.5f);
+        float _fy = Mathf.Floor(target.y + 0.5f);
+
+        Debug.Log(_fx + " " + _fy);
+    }
+}
